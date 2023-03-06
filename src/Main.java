fc3c562d0d4d47f4ae9f3864c1a4043d70539dc8
IlyuAfanasev <gecktor.86@mail.ru>
2023-03-06 16:27:11 +0400

    correct task 1

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,25 @@ public class Main {
         numbers1[1]=2;
         numbers1[2]=3;
         for (int i = 0; i < 3; i++) {
-            System.out.print(numbers1[i]+" ");
+            System.out.print(numbers1[i] + " ");
         }
+        System.out.println();
+            double []numbers2 = {1.57,7.654,9.986};
+        for (int i = 0; i < 3; i++) {
+            System.out.print(numbers2[i] + " ");
+        }
+        System.out.println();
+        int[] numbers3 = new int[10];
+        int j=1;
+        int i = 0;
+        while (i < numbers3.length ) {
+            numbers3[i]=0;
+            numbers3[j]=1;
+            System.out.print(numbers3[i]+" ");
+            System.out.print(numbers3[j]+" ");
+            j+=2;
+            i+=2;
+        }
+        System.out.println();
     }
 }
