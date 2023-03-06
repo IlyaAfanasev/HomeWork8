eff4c95f17eef816e95306f6de5a12665f6d4738
IlyuAfanasev <gecktor.86@mail.ru>
2023-03-06 16:21:36 +0400

    task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("    Задача 1");
+        int []numbers1 = new int [3];
+        numbers1[0]=1;
+        numbers1[1]=2;
+        numbers1[2]=3;
+        for (int i = 0; i < 3; i++) {
+            System.out.print(numbers1[i]+" ");
+        }
     }
 }
